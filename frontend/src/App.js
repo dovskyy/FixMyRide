fd34cb2da03536e773156e3787666f719b2f3473
dovskyy <marcelin920@gmail.com>
2024-07-23 23:16:48 +0200

    frontend basic config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/employees')
+        .then(response => {
+          setEmployees(response.data);
+        })
+        .catch(error => {
+          console.error('There was an error fetching the employees!', error);
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Employee List</h1>
+          <ul>
+            {employees.map(employee => (
+                <li key={employee.id}>{employee.name}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
   );
 }
 
